dd7e076116dcf0c0d63ee67b2803cbedfed391b7
arsen.demirci <arsen.demirci@eczacibasi.com.tr>
2022-10-09 14:29:34 +0300

    fixed axios api

--- src/api/http/axiosApi.js
+++ src/api/http/axiosApi.js
@@ -10,7 +10,12 @@ class AxiosApi {
   postData(url, data) {
     return this.http.post(`${this.baseUrl}${url}`, data);
   }
-  getData(url) {
-    return this.http.get(`${this.baseUrl}${url}`);
+  async getData(url) {
+    const response = await this.http.get(`${this.baseUrl}${url}`);
+    if (response.statusText == "OK" && response.data) {
+      return response.data;
+    } else {
+      return [];
+    }
   }
 }
