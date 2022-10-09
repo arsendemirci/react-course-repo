37e63f0a8988152f56d7f02e7e720ba771f79292
arsen.demirci <arsen.demirci@eczacibasi.com.tr>
2022-10-09 20:19:44 +0300

    added task project

--- src/components/Brand/BackwardCounter.js
+++ src/components/Brand/BackwardCounter.js
@@ -1,4 +1,4 @@
-import useCounter from "../hooks/useCounter";
+import useCounter from "../../hooks/useCounter";
 import { Card } from "@components";
 
 const BackwardCounter = () => {
--- src/components/index.js
+++ src/components/index.js
@@ -1,2 +1,9 @@
 export { default as Button } from "./UI/Button/Button";
 export { default as Card } from "./UI/Card";
+export { default as NewTask } from "./Brand/NewTask/NewTask";
+export { default as TaskForm } from "./Brand/NewTask/TaskForm";
+export { default as TaskItem } from "./Brand/Tasks/TaskItem";
+export { default as Tasks } from "./Brand/Tasks/Tasks";
+export { default as NewTask } from "./Brand/NewTask/NewTask";
+export { default as BackwardCounter } from "./Brand/BackwardCounter";
+export { default as ForwardCounter } from "./Brand/ForwardCounter";
