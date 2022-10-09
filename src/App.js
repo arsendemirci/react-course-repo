970860f154a1f5125a74d09fe32390fed8826bfe
arsen.demirci <arsen.demirci@eczacibasi.com.tr>
2022-10-09 20:06:49 +0300

    merged master

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
-<<<<<<< HEAD
-import React from 'react';
-import BackwardCounter from './components/BackwardCounter';
-import ForwardCounter from './components/ForwardCounter';
+import React from "react";
+import BackwardCounter from "./components/BackwardCounter";
+import ForwardCounter from "./components/ForwardCounter";
 
 function App() {
   return (
@@ -10,16 +9,6 @@ function App() {
       <BackwardCounter />
     </React.Fragment>
   );
-=======
-import React from "react";
-import useFetchApi from "./hooks/useFetchApi";
-
-function App() {
-  // const [data] = useFetchApi("movies.json");
-  // console.log(data);
-
-  return <div>Welcome To React</div>;
->>>>>>> master
 }
 
 export default App;
