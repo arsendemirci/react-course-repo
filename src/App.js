8910bf97d742d9d9d3e7aef8ae064088bff59ef6
arsen.demirci <arsen.demirci@eczacibasi.com.tr>
2022-10-12 21:44:04 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,10 @@
-import React, { useState, useEffect } from "react";
-import { useAxiosApi } from "@hooks";
-import { taskApi } from "@services";
+import SimpleInput from './components/SimpleInput';
 
 function App() {
-  const [tasks, setTasks] = useState([]);
-  const { isLoading, error, sendRequest: getTasks } = useAxiosApi();
-
-  useEffect(() => {
-    const loadTasks = (tasks) => {
-      setTasks(taskApi.loadTasks(tasks));
-    };
-    getTasks({ url: "tasks.json" }, loadTasks);
-  }, [getTasks]);
   return (
-    <React.Fragment>
-      <div>Welcome To React</div>
-      <ul>
-        {isLoading && <p>Loading Data Please Wait...</p>}
-        {!isLoading && tasks.length
-          ? tasks.map((task) => <li key={task.id}>{task.title}</li>)
-          : null}
-        {!isLoading && error && <p>{error}</p>}
-      </ul>
-    </React.Fragment>
+    <div className="app">
+      <SimpleInput />
+    </div>
   );
 }
 
